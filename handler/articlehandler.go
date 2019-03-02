6b84c60a46c31e7532a980f916f1f44dcf50f3c1
vinhha <vinhha@vng.com.vn>
2019-03-02 22:03:55 +0700

    [App] Update save/fetch article in DB

--- handler/articlehandler.go
+++ handler/articlehandler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	"github.com/vinhha96/golang-research/models"
@@ -18,7 +19,7 @@ func NewArticleHandler(db *gorm.DB) *ArticleHandler {
 
 func (articleHandler *ArticleHandler) GetArticleByID(ctx *gin.Context) {
 	if articleID, err := strconv.Atoi(ctx.Param("article_id")); err == nil {
-		if article, err := models.GetArticleByID(articleID); err == nil {
+		if article, err := articleHandler.GetArticleByIDFromDB(articleID); err == nil {
 			render(ctx, gin.H{
 				"title":   article.Title,
 				"payload": article,
@@ -41,7 +42,7 @@ func (articleHandler *ArticleHandler) CreateNewArticle(ctx *gin.Context) {
 	title := ctx.PostForm("title")
 	content := ctx.PostForm("content")
 
-	if article, err := models.CreateNewArticle(title, content); err == nil {
+	if article, err := articleHandler.CreateNewArticleInDB(title, content); err == nil {
 		render(ctx, gin.H{
 			"title":   "Submission successfuly",
 			"payload": article,
@@ -49,5 +50,28 @@ func (articleHandler *ArticleHandler) CreateNewArticle(ctx *gin.Context) {
 	} else {
 		ctx.AbortWithStatus(http.StatusBadRequest)
 	}
+}
 
+func (articleHandler *ArticleHandler) GetArticleByIDFromDB(articleID int) (*models.Article, error) {
+	var articleList []models.Article
+
+	articleHandler.db.Find(&articleList)
+
+	for _, article := range articleList {
+		if article.ID == articleID {
+			return &article, nil
+		}
+	}
+	return nil, errors.New("Can't find article in database")
+}
+
+func (articleHandler *ArticleHandler) CreateNewArticleInDB(title, content string) (*models.Article, error) {
+	var articleList []models.Article
+	articleHandler.db.Find(&articleList)
+
+	newArticle := models.Article{ID: len(articleList) + 1, Title: title, Content: content}
+
+	articleList = append(articleList, newArticle)
+
+	return &newArticle, nil
 }
--- models/articles.go
+++ models/articles.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	"errors"
 	"time"
 )
 
@@ -13,34 +12,4 @@ type Article struct {
 
 	Title   string `json:"title"`
 	Content string `json:"content"`
-}
-
-// For this demo, we're storing the Article list in memory
-// In a real application, this list will most likely be fetched
-// from a database or from static files
-var articleList = []Article{
-	{ID: 1, Title: "Article 1", Content: "Article 1 body"},
-	{ID: 2, Title: "Article 2", Content: "Article 2 body"},
-}
-
-// Return a list of all the articles
-func GetAllArticles() []Article {
-	return articleList
-}
-
-func GetArticleByID(articleID int) (*Article, error) {
-	for _, article := range articleList {
-		if article.ID == articleID {
-			return &article, nil
-		}
-	}
-	return nil, errors.New("Can't find article in database")
-}
-
-func CreateNewArticle(title, content string) (*Article, error) {
-	newArticle := Article{ID: len(articleList) + 1, Title: title, Content: content}
-
-	articleList = append(articleList, newArticle)
-
-	return &newArticle, nil
 }
